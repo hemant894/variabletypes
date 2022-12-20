9de0ef4e2cc0f3637ead308387a636b8abaaafef
Hemant <hemant894@gmail.com>
2022-12-20 18:49:38 +0000

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,31 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // integer family
+        // byte range  is -128 to 127
+        byte a = -100;
+        System.out.println("a;" +a);
+        //  long range
+        long b= 1000;
+        System.out.println("b;" +b);
+        // short rang
+        short c= 2500;
+        System.out.println("c;" +c);
+        // int
+        int d = 25000;
+        System.out.println("d;"+d);
+        // float family
+        float e = 1245;
+        System.out.println("e;" + e);
+        double f = -12356;
+        System.out.println("f;" +f);
+        // character family
+        char g = '1';
+        System.out.println("g;" +g);
+        // boolean family
+        boolean h = false;
+        System.out.println("h:" +h);
+
+
+
+    }
+}
